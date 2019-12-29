6a0618655318cf6c13c60db0c8b697c781a92433
Michael A. Ventarola <mikev1963@me.com>
2019-12-29 17:30:02 -0500

    Updated useradd.sh

--- useradd.sh
+++ useradd.sh
@@ -33,5 +33,7 @@ do
         usermod -aG ${SUDO_GROUP} ${user}
         sed -i "/%${SUDO_GROUP}.*NOPASSWD/s/^#\s*//g" /etc/sudoers
         echo "${user}:${user}" | chpasswd
+        mkdir -p /home/${user}/.ssh
+        echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCk8J1hk7fnHFQ+IJBiBafYlps9z/j4oNbxChWZzCZ80ZB6p6r/6XORHIbb90Kh6d/+meMXVuMNrRQSbDyxoEAS7RarDuADYm2DhOxdxueecwnvBR2+NdGBeixxDjldWvPNa5/mto3CWA77mc9qdYj42s2fY8p8RvtPCNM6ShfPiCUXQrHydUZIdqqmltsElZ5gqKlI7DbGdWFJcxM1+aeaY78rmw+WZyDeMeEtER7ko2v5vqg44QdIKKyOHW/KaLZjYyGJFHz+LsbcSl4ED1Ds9f9Fp+M4a79GVNfOnw8FM1UGJDqhYW6XG/zzkOhb2gOgKcZPC1mQLyxAEe1GJ7TP mikev1963@Michaels-MBP.fios-router.home" >> /home/${user}/.ssh/authorized_keys
 done
 
